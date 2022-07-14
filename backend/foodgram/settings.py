fa39aa1432980cf1d20c6077091d5330d52e4c18
Раиль Ибятов <rtibyatov@gmail.com>
2022-07-14 22:49:29 +0300

    starting deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]  # "localhost", "127.0.0.1", "backend"
 
 
 # Application definition
@@ -141,7 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
